5d0450ade5c543c511975a0540217104100e46be
Diogo Loiola <diogomouraloiola@gmail.com>
2023-01-26 20:15:45 -0300

    create AppHeader

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
-  <AppUploadFiles />
+  <AppHeader />
+
+  <main class="container">
+    <section>
+      <div class="mt-3">
+        <h3>Conversor de audio</h3>
+      </div>
+    </section>
+
+  </main>
 </template>
 
 <script setup lang="ts">
-  import AppUploadFiles from './components/AppUploadFiles.vue';
+import AppHeader from './components/AppHeader.vue';
 </script>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,36 @@
+<template>
+    <header class="bg-black">
+        <div class="container d-flex">
+            <h1 class="site-title">General app</h1>
+            <nav class="nav">
+                <ul class="nav-header">
+                    <li>
+                        <a href="">Conversores</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<style scoped>
+.site-title {
+    color: #fff;
+    font-size: 1.5rem;
+    font-weight: bold;
+}
+
+.nav-header {
+    margin-left: 1rem;
+}
+
+.nav-header li {
+    padding: 1rem 1.4rem;
+}
+
+.nav-header li a {
+    color: var(--white);
+    text-decoration: none;
+    font-weight: 100;
+}
+</style>
